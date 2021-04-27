d95c26aaef060f2312b2f68bba2f8de421c3565e
Gustavo Azevedo <gustavosazevedo@gmail.com>
2021-04-27 14:25:06 -0300

    refactoring dashboard

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,44 @@
+const Job = require("../model/Job")
+const Profile = require("../model/Profile")
+const JobUtils = require("../utils/JobUtils")
+
+module.exports = {
+  index: (req, res) => {
+    const jobs = Job.get();
+    const profile = Profile.get();
+
+    let statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
+    let jobTotalHours = 0
+
+    const updatedJobs = jobs.map((job) => {
+      // Ajustes no job
+      const remaining = JobUtils.remainingDays(job);
+      const status = remaining <= 0 ? 'done' : 'progress';
+    
+      // Somando a quantidade de status
+      statusCount[status] += 1;
+
+      jobTotalHours = status == 'progress' ? jobTotalHours += Number(job["daily-hours"]) : jobTotalHours
+      // if(status == 'progress') {
+      //   jobTotalHours += Number(job["daily-hours"])
+      // }
+
+      return {
+        ...job,
+        remaining,
+        status,
+        budget: JobUtils.calculateBudget(job, profile["value-hour"])
+      }
+    })
+
+    // quantidade de horas que quero trabalhar/dia (profile) - quantidade de horas/dia de cada job em progress
+    const freeHours = profile["hours-per-day"] - jobTotalHours;
+
+    return res.render('index', { jobs: updatedJobs, profile, statusCount, freeHours })
+  }
+}
